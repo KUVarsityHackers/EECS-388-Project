c2e3fbd4e266edd1bbd1084c22329ff74c90a16b
Michael Svoren <msvoren@ku.edu>
2019-05-01 09:48:35 -0500

    beatufiy website

--- submitData.php
+++ submitData.php
@@ -4,6 +4,7 @@
 
 
 $distance = $_GET['distance'];
+$setupDistance = $_GET['setup'];
 
 // set default timezone
 date_default_timezone_set('America/Chicago'); // CDT
@@ -26,9 +27,17 @@ if ($mysqli->connect_errno)
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-
+echo "$initVal";
 
 $insert = "INSERT INTO Trash_Tech (time, distance) VALUES('$current_date', '$distance');";
+$setInitial = "INSERT INTO TRASH_TECH_INIT (initial) VALUES ($setupDistance);";
+$drop = "DELETE FROM TRASH_TECH_INIT;";
+
+if (!empty($setupDistance))
+{
+    $mysqli->query($drop);
+    $mysqli->query($setInitial);
+}
 
 if (!empty($distance))
 {
@@ -36,15 +45,30 @@ if (!empty($distance))
 }
 
 $query = "SELECT distance, time FROM Trash_Tech ORDER BY id DESC LIMIT 1;";
+$getSize = "SELECT initial FROM TRASH_TECH_INIT LIMIT 1;";
 
 $sql = mysqli_query($mysqli, $query);
 $result = $sql->fetch_assoc();
 $takenDistance = $result['distance'];
 $takenDate = $result['time'];
 
+$sqlInitial = mysqli_query($mysqli, $getSize);
+$resultInitial = $sqlInitial->fetch_assoc();
+$initVal = $resultInitial['initial'];
+$percentage = $takenDistance / $initVal*100;
+echo "<div style=\"margin: 0 auto; width: 50%;text-align: center\">";
+echo "<head> <h1> Trash Tech - Home </h1> </head>";
+echo "<body style=\"background-color: lightseagreen \">";
 
-echo "Distance: $takenDistance <br>";
+if($percentage>=90)
+{
+  echo "<script> alert(\"Time to empty the trash!\")</script> <br>";
+}
+
+echo "Percentage Full: $percentage% <br>";
 echo "Date: $takenDate";
+
+echo "</body>";
 
 
 $mysqli->close();
